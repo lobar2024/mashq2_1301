d6f1877c1ebcef5612aa649e33cf454d19f8d052
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-01-13 15:35:14 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+a = int(input("a ni kiriting: "))
+b = int(input("b ni kiriting: "))
+
+print("Yig‘indi:", a + b)
+print("Ayirma:", a - b)
+print("Ko‘paytma:", a * b)
+
+if b != 0:
+    print("Bo‘linma:", a / b)
+else:
+    print("0 ga bo‘lib bo‘lmaydi")
